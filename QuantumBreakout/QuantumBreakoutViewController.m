24b3e3f8a1daca1456e6af091c51127e01bb1c60
stansbury <sarahstansbury@169-231-93-44.wireless.ucsb.edu>
2013-10-17 18:27:13 -0700

    Ball center bounces off the sides of the application

--- QuantumBreakout/QuantumBreakoutViewController.m
+++ QuantumBreakout/QuantumBreakoutViewController.m
@@ -20,12 +20,24 @@
 
 - (void)AnimateBall{
     ball.center = CGPointMake(ball.center.x + dx, ball.center.y + dy);
+    //handle collisions
+    
+    //temporary values--need to find robust way of getting boundaries of application
+    int width = self.view.center.x * 2;
+    int height = self.view.center.y * 2;
+    
+    if(ball.center.x > width || ball.center.x < 0){
+        dx = -dx;
+    }
+    if(ball.center.y > height || ball.center.y < 0){
+        dy = -dy;
+    }
+
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     //initialize timer
     timer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(AnimateBall) userInfo:nil repeats:YES];
     //initialize dx and dy
